d20d4efe7db277fa27be0fc5a7e3efe678f3fc36
GSUser <GSUser@MSI>
2025-08-01 17:18:06 +0900

    0801

--- StageScene.cpp
+++ StageScene.cpp
@@ -1,12 +1,23 @@
 #include "StageScene.h"
 #include <Novice.h>
 
-void StageScene::Initialize() {}
+void StageScene::Initialize() {
+	playerPosx = 200.0f;
+	playerPosy = 200.0f;
+
+}
 
 void StageScene::Update(char* keys, char* preKeys) {
 
+	if (keys[DIK_D]) {
+		playerPosx += speed_;
+	}
+	if (preKeys[DIK_A]) {
+		playerPosx -= speed_;
+	}
+
 	// スペースキーで次のシーンへ
-	if (preKeys[DIK_SPACE] == 0 && keys[DIK_SPACE] != 0) {
+	if (playerPosx >= 700) {
 
 		sceneNo = CLEAR;
 	}
@@ -15,5 +26,6 @@ void StageScene::Update(char* keys, char* preKeys) {
 void StageScene::Draw() {
 
 	Novice::ScreenPrintf(0, 0, "Curernt Scene : Stage");
-	Novice::ScreenPrintf(0, 30, "Push Space : Next Scene");
+	Novice::DrawBox((int)playerPosx,(int) playerPosy, 100, 100, 0.0f, WHITE, kFillModeWireFrame);
+	Novice::DrawBox(800,200, 50, 50, 0.0f, BLACK, kFillModeSolid);
 }
--- StageScene.h
+++ StageScene.h
@@ -1,11 +1,24 @@
 #pragma once
 #include "IScene.h"
+#include "math/Vector2.h"
+
+using namespace KamataEngine;
+
 
 class StageScene : public IScene {
 
 public:
 
+	// 位置
+	float playerPosx;
+	float playerPosy;
+
+	// 速さ
+	float speed_ = 10.0f;
+
+
 	void Initialize() override;
 	void Update(char* keys, char* preKeys) override;
 	void Draw() override;
+
 };
